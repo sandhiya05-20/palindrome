9a5c7a938aeb33b3fb99c7429055b95be14246c0
Sandhiya <ss3317@srmist.edu.in>
2026-03-09 23:11:25 +0530

    Added UC13 - Performance comparison using nanoTime

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,6 +3,43 @@ import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
+    // Method 1: Using reverse string
+    public static boolean checkUsingReverse(String input) {
+        String reversed = new StringBuilder(input).reverse().toString();
+        return input.equalsIgnoreCase(reversed);
+    }
+
+    // Method 2: Using stack
+    public static boolean checkUsingStack(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        return input.equalsIgnoreCase(reversed);
+    }
+
+    // Method 3: Using two pointers
+    public static boolean checkUsingTwoPointers(String input) {
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -10,25 +47,25 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a word: ");
         String input = scanner.nextLine();
 
-        Stack<Character> stack = new Stack<>();
+        // Reverse method timing
+        long start1 = System.nanoTime();
+        boolean result1 = checkUsingReverse(input);
+        long end1 = System.nanoTime();
 
-        // push characters to stack
-        for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
-        }
+        // Stack method timing
+        long start2 = System.nanoTime();
+        boolean result2 = checkUsingStack(input);
+        long end2 = System.nanoTime();
 
-        String reversed = "";
+        // Two pointer timing
+        long start3 = System.nanoTime();
+        boolean result3 = checkUsingTwoPointers(input);
+        long end3 = System.nanoTime();
 
-        // pop characters to build reversed string
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
-        }
-
-        if (input.equalsIgnoreCase(reversed)) {
-            System.out.println("It is a Palindrome");
-        } else {
-            System.out.println("Not a Palindrome");
-        }
+        System.out.println("\nResults:");
+        System.out.println("Reverse Method: " + result1 + " Time: " + (end1 - start1) + " ns");
+        System.out.println("Stack Method: " + result2 + " Time: " + (end2 - start2) + " ns");
+        System.out.println("Two Pointer Method: " + result3 + " Time: " + (end3 - start3) + " ns");
 
         scanner.close();
     }
